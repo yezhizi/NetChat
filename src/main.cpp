566165806b5fef9a55a14b9e26b27a39aaafef0c
Nativu5 <sdtalyk@163.com>
2023-12-14 16:06:14 +0800

    :recycle: Recreate CMakeList

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "Client.h"
 #include "Server.h"
 #include "logging.h"
 #include "user_manager.h"
@@ -8,33 +7,11 @@ using namespace ntc;
 
 int main(int argc, char *argv[]) {
     el::configureServerLogger();
-    // LOG(DEBUG) << "My first info log using default logger";
+    LOG(DEBUG) << "My first info log using default logger";
 
     Server *server = Server::Get();
     UM::Get()->setUserInfo("123", 1, "123456");
     UM::Get()->setUserInfo("456", 2, "123456");
-    Client *client1 = new Client();
-    Client *client2 = new Client();
-
-    std::thread t1([&]() {
-        client1->sendServerStatusRequest();
-        client2->sendServerStatusRequest();
-        client1->sendServerStatusUpdateRequest();
-        client2->sendServerStatusUpdateRequest();
-        client1->login("123", "123456");
-        client2->login("456", "123456");
-        for (int i = 0; i < 10; ++i) {
-            client1->sendServerStatusRequest();
-            client2->sendServerStatusRequest();
-            client1->sendServerStatusUpdateRequest();
-            client2->sendServerStatusUpdateRequest();
-        }
-        client1->setupChannel();
-        client2->setupChannel();
-    });
-    t1.join();
-    delete client1;
-    delete client2;
 
     return 0;
 }
