e626ed0c47759ce7917a10e49e91c5a9bc9ea8a4
Nativu5 <sdtalyk@163.com>
2023-12-16 15:15:29 +0800

    :sparkles: Add auto create DB

--- include/db.h
+++ include/db.h
@@ -10,11 +10,9 @@ class DataAccess {
   SQLite::Database db;
 
  public:
-  // 构造函数
+  // 构造函数，自动创建数据库和相关结构
   DataAccess(const std::string& dbPath = "chat.db")
       : db(dbPath, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE) {
-    // 打开数据库连接
-    // 检查并创建表
     checkAndCreateTables();
   }
 
@@ -31,7 +29,6 @@ class DataAccess {
           "CREATE TABLE users ("
           "user_id INTEGER PRIMARY KEY AUTOINCREMENT, "
           "username TEXT NOT NULL UNIQUE, "
-          "display_name TEXT, "
           "email TEXT, "
           "phone TEXT)");
     }
@@ -54,6 +51,34 @@ class DataAccess {
           "FOREIGN KEY (user_id) REFERENCES users (user_id), "
           "FOREIGN KEY (group_id) REFERENCES groups (group_id), "
           "PRIMARY KEY (user_id, group_id))");
+    }
+
+    // 检查并创建文件表
+    if (!db.tableExists("files")) {
+      db.exec(
+          "CREATE TABLE files ("
+          "file_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "filename TEXT NOT NULL, "
+          "hash TEXT NOT NULL, "
+          "timestamp INTEGER NOT NULL)");
+    }
+
+    // 检查并创建聊天记录表
+    if (!db.tableExists("messages")) {
+      db.exec(
+          "CREATE TABLE messages ("
+          "message_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "sender_id INTEGER NOT NULL, "
+          "receiver_id INTEGER NOT NULL, "
+          "type INTEGER NOT NULL, "  // oneof TEXT, IMAGE or FILE
+          "internal_id INTEGER, "    // 用于标识对话内部的消息
+          "content TEXT NOT NULL, "  // TEXT/IMAGE 类型的消息内容
+          "file_id INTEGER, "        // FILE 类型外键
+          "hash TEXT NOT NULL, "
+          "timestamp INTEGER NOT NULL, "
+          "FOREIGN KEY (sender_id) REFERENCES users (user_id), "
+          "FOREIGN KEY (receiver_id) REFERENCES users (user_id), "
+          "FOREIGN KEY (file_id) REFERENCES files (file_id))");
     }
   }
 
