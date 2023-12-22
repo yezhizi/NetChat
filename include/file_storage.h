7a6db4bf3df0553b0d986909ace7b935351114bb
Nativus <44155313+Nativu5@users.noreply.github.com>
2023-12-22 01:07:31 +0000

    :sparkles: Implement filestorage

--- include/file_storage.h
+++ include/file_storage.h
@@ -1,6 +1,11 @@
 #ifndef FILE_STORAGE_H
 #define FILE_STORAGE_H
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
 #include "message.h"
+#include "easylogging++.h"
 
 namespace ntc::file {
 class FileStorage {
@@ -11,13 +16,11 @@ class FileStorage {
   // Getters
   std::string getId() const { return id_; }
   std::string getName() const { return name_; }
-  std::string getPath() const { return path_; }
   std::string getHash() const { return hash_; }
 
   // Setters
   void setId(std::string id) { id_ = id; }
   void setName(std::string name) { name_ = name; }
-  void setPath(std::string path) { path_ = path; }
   void setHash(std::string hash) { hash_ = hash; }
 
   // Methods
@@ -28,8 +31,9 @@ class FileStorage {
  private:
   std::string id_;    // UUID
   std::string name_;  // filename on client side
-  std::string path_;  // path to the file
   std::string hash_;  // hash of the file
+
+  const std::filesystem::path path_prefix_{"files"};
 };
 }  // namespace ntc::file
 
--- src/file/file_storage.cpp
+++ src/file/file_storage.cpp
@@ -3,14 +3,40 @@
 
 namespace ntc::file {
     bool FileStorage::checkDiskExistence() {
-        return false;
+        auto p = path_prefix_ / std::filesystem::path(id_);        
+
+        return std::filesystem::exists(p);
     }
 
     bool FileStorage::saveToDisk(const netdesign2::File &file) {
-        return false;
+        auto p = path_prefix_ / std::filesystem::path(id_);
+
+        std::ofstream ofs(p, std::ios::binary);
+        if (!ofs.is_open()) {
+            return false;
+        }
+
+        ofs.write(file.content().data(), file.content().size());
+        if (!ofs.good()) {
+            LOG(ERROR) << "Failed to write file to disk";
+            return false;
+        }
+        
+        return true;
     }
 
     bool FileStorage::loadIntoProto(netdesign2::File &file) {
-        return false;
+        auto p = path_prefix_ / std::filesystem::path(id_);
+
+        std::ifstream ifs(p, std::ios::binary);
+        if (!ifs.is_open()) {
+            return false;
+        }
+
+        std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+        file.set_content(content);
+        file.set_name(name_);
+
+        return true;
     }
 }
